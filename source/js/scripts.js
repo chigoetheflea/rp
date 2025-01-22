145167def911ce8ef3d30885717e4fb8def43620
chigoetheflea <chigoetheflea@gmail.com>
2025-01-22 16:19:58 +0300

    Вносит правки

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -131,6 +131,53 @@ const toggleClasses = (element, elClasses) => {
   });
 };
 
+const setCookie = (name, value, options = {}) => {
+  options = {
+    path: '/',
+    ...options
+  };
+
+  if (options.expires instanceof Date) {
+    options.expires = options.expires.toUTCString();
+  }
+
+  let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+  for (let optionKey in options) {
+    updatedCookie += "; " + optionKey;
+    let optionValue = options[optionKey];
+    if (optionValue !== true) {
+      updatedCookie += "=" + optionValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+};
+
+const getCookie = (name) => {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+/* calltouch cookie */
+
+const CALLTOUCH_COOKIE = `_ct_session_id`;
+let ct = null;
+
+if (window.ct) {
+  ct = window.ct('calltracking_params')[0];
+  ct = ct.sessionId;
+} else {
+  ct = Date.now();
+}
+
+setCookie(CALLTOUCH_COOKIE, ct, {'max-age': 7200, 'samesite': 'strict'});
+
+/* calltouch cookie */
+
 document.addEventListener(`DOMContentLoaded`, function() {
 
   setTimeout(() => {
@@ -483,6 +530,10 @@ document.addEventListener(`DOMContentLoaded`, function() {
   };
 
   /* __ form sending */
+
+  // const sendCallTouchData = () => {
+
+  // };
 
   /* __ form check */
 
